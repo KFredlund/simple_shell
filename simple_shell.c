2e80e60ba92fa5096d444a442cd8a8129c143526
KFredlund <katihomes@gmail.com>
2019-11-15 22:22:34 +0000

    Create main function for simple shell

---
+++ simple_shell.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* main - Entry point, main function
+*
+* Return: 0 if success, else 1
+*/
+int main(int argc, char *argv[])
+{
+// loop while not exit
+	// print prompt
+	//getline
+	//parse command - strtok
+	//execute args
+	// free line
+	// free args
+}
