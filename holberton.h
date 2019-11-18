95cca1bba0376aed1b1f21f463c67636db6facb2
KFredlund <katihomes@gmail.com>
2019-11-18 15:02:28 +0000

    Create header file with function prototypes and libraries

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+int _putchar(char c);
+ssize_t _getline(char *line);
+#endif
