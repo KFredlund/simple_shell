1ce3c9e37fa5837b4132803339cdd6c1ebf46298
Kathleen McKiernan <851@holbertonschool.com>
2019-11-19 16:55:05 +0000

    move malloc functionality to allocation helper

--- shell.c
+++ shell.c
@@ -6,35 +6,7 @@
 */
 int main(int argc, char *argv[])
 {
-	int buffsize = LSH_RL_BUFSIZE;
-	int location = 0, count = 0, parse;
-	char *buffer = malloc(sizeof(char) * buffsize);
-
-/* checking if buffer allocates desired memory */
-	if (!buffer)
-	{
-		bufferror();
-	}
-/* loop while not exit */
-	while (count)
-	{
-		count = open(buffer, O_RDONLY);
-		if (count == -1)
-		{
-			return(0);
-		}
-		else if (parse = read(count, buffer);
-	}
-/* check buffer space */
-		if (location >= buffsize)
-		{
-			buffsize += LSH_RL_BUFSIZE;
-			buffer = _realloc(buffer, buffsize);
-			if (!buffer)
-			{
-				bufferror();
-			}
-		}
+/* if buffer mallocs */
 
 	// print prompt
 
