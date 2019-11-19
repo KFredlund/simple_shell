45a210c7a193aff02f3db3e2ac3e7bce33a3cd31
Kathleen McKiernan <851@holbertonschool.com>
2019-11-19 16:11:02 +0000

    add some malloc structure into main

--- shell.c
+++ shell.c
@@ -6,10 +6,40 @@
 */
 int main(int argc, char *argv[])
 {
-// loop while not exit
+	int buffsize = LSH_RL_BUFSIZE;
+	int location = 0, count = 0, parse;
+	char *buffer = malloc(sizeof(char) * buffsize);
+
+/* checking if buffer allocates desired memory */
+	if (!buffer)
+	{
+		bufferror();
+	}
+/* loop while not exit */
+	while (count)
+	{
+		count = open(buffer, O_RDONLY);
+		if (count == -1)
+		{
+			return(0);
+		}
+		else if (parse = read(count, buffer);
+	}
+/* check buffer space */
+		if (location >= buffsize)
+		{
+			buffsize += LSH_RL_BUFSIZE;
+			buffer = _realloc(buffer, buffsize);
+			if (!buffer)
+			{
+				bufferror();
+			}
+		}
+
 	// print prompt
+
 	//getline
-	//parse command - strtok
+/* call to strtok function */
 	//execute args
 	// free line
 	// free args
