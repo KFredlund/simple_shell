b57f64ea4578108d554261d5bf901a89ceaa1473
KFredlund <katihomes@gmail.com>
2019-11-20 15:48:33 +0000

    Add prompt functionality using write and read

--- shell.c
+++ shell.c
@@ -4,12 +4,20 @@
 *
 * Return: 0 if success, else 1
 */
-int main(int argc, char *argv[])
+int main(void)
 {
 /* if buffer mallocs */
-
-	// print prompt
-
+	char *prompt = "Team Kati^2e Shell $";
+	int status = 0;
+	char *line[1000];
+	
+	while (status < 2)
+	{
+		write(1, prompt, strlen(prompt) + 1);
+		read(0, line, 1000);	
+		status++;
+	}
+	return (0);
 	//getline
 /* call to strtok function */
 	//execute args
