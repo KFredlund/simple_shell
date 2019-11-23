bb21280c2803dd328c22f5b6cfbfb56f77c7b794
KFredlund <katihomes@gmail.com>
2019-11-23 17:42:37 +0000

    Added function for executing arguments

--- shell.c
+++ shell.c
@@ -2,6 +2,7 @@
 #include <signal.h>
 int _strcmp(char *s1, char *s2);
 void cont_c_help();
+int exec(char *args);
 /**
 * main - Entry point, main function
 *
@@ -32,14 +33,15 @@ int main(void)
 		else
 		{
 			token = strtok(buf, " ");
-			while (token)			
+			status = exec(token);
+/*			while (token)			
 			{
 				write(1, token, strlen(token) + 1);
 				write(1, nl, strlen(nl) + 1);
 				token = strtok(NULL, " ");
 			}
 			status++;
-		}
+*/		}
 	}
 	free(buf);
 	free(token);
@@ -78,4 +80,57 @@ int _strcmp(char *s1, char *s2)
 		l++;
 	}
 	return (s1[l] - s2[l]);
+}
+/**
+ * exec - Execute command function
+ * @args: arguments to execute
+ * 
+ * Return: number of args executed
+ */
+int filter(char *token)
+{
+	int i = 0, switch_cmnd = 0;
+	char *builtin_list[6];
+
+        builtin_list[0] = "cd";
+        builtin_list[1] = "help";
+        builtin_list[2] = "history";
+        builtin_list[3] = "env";
+        builtin_list[4] = "setenv";
+        builtin_list[5] = "unsetenv";
+
+	if (token[0] == NULL)
+		return (1);
+	for (; i < 6; i++)
+	{
+		if (_strcmp(token[0], builtin_list[i]))
+		{
+			switch_cmnd = i + 1;
+			break;
+		}
+	}
+	switch(switch_cmnd) 
+	{
+	case 1:
+		chdir(token[1]);
+		return (1);
+	case 2:
+		helpfunc();
+		return (1);
+	case 3:
+		histfunc();
+		return (1);
+	case 4:
+		environment();
+		return (1);
+	case 5:
+		setenvironment();
+		return (1);
+	case 6:
+		unsetenvironment();
+		return (1);
+	default:
+		break;
+	}
+	return (0);
 }
